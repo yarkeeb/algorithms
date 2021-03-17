b9dd855dded73c234aae687569fbe7e210ccce24
Yaroslav Romanov <yar.a.romanov@gmail.com>
2021-03-17 20:52:03 +0300

    ref

--- dynamic_programming/climb_stairs.go
+++ dynamic_programming/climb_stairs.go
@@ -1,5 +1,6 @@
 package dynamic_programming
 
+//Leetcode 70
 //You are climbing a staircase. It takes n steps to reach the top.
 //
 //Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
@@ -24,7 +25,6 @@ package dynamic_programming
 //Constraints:
 //1 <= n <= 45
 //
-
 
 func ClimbStairs(n int) int {
 	if n == 1 {
